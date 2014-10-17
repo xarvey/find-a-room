c93ec1baa18735073ed31195dcefb4493a41ec72
zmai@purdue.edu <zmai@purdue.edu>
2014-10-16 21:57:41 -0400

    Owen's commit

---
+++ src/stringToNode.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="demo"></p>
+
+<script>
+function myFunction(str) {
+    var split = str.split("_");
+    var Node = {
+        bldg : split[0],
+        floor : split[1],
+        num : split[2],
+	};
+	return Node;
+}
+
+</script>
+
+</body>
+</html>
