5435ad62fd042d7e2b7d461ca3b1af7cc0be7c3a
igodzp <saranyu_phusit@hotmail.com>
2014-11-05 02:17:53 -0500

    Improve QR code scanner

--- findaroom/findaroom.js
+++ findaroom/findaroom.js
@@ -1,5 +1,27 @@
 var IMAGE_KEY = "qrcode-img";
 
+var gCtx = null;
+var gCanvas = null;
+
+function initCanvas(w,h)
+{
+    gCanvas = document.getElementById("qr-canvas");
+    gCanvas.style.width = w + "px";
+    gCanvas.style.height = h + "px";
+    gCanvas.width = w;
+    gCanvas.height = h;
+    gCtx = gCanvas.getContext("2d");
+    gCtx.clearRect(0, 0, w, h);
+    console.log("done");
+}
+
+function load()
+{
+  console.log("YEAH");
+  initCanvas(800,600);
+}
+
+
 // simple-todos.js
 if (Meteor.isClient) {
   // This code only runs on the client
@@ -11,16 +33,21 @@ if (Meteor.isClient) {
     ]
   });
 
+  Meteor.startup(function () {
+    load();
+  });
+
   Template.home.events({
     'click .scan-qr': function() {
-      qrcode.callback = function(result){
-          alert(result);
-      };
+
       MeteorCamera.getPicture({width: 320}, function(error, data) {
         if (error)
           alert(error.reason);
         else{
-          Session.set(IMAGE_KEY, data);
+          qrcode.callback = function(result){
+              alert(result);
+          };
+          gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
           qrcode.decode(data);
         }
       });
